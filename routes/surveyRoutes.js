622ef476fd11dfd5a600029ea017c0022b335ccd
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-21 00:46:58 +0545

    create instance of survey route : POST

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,25 @@
+const mongoose = require('mongoose')
+
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const Mailer = require('../services/Mailer')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+
+const Survey = mongoose.model('Survey')
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {})
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body
+
+		const survey = {
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })), // return array of object with email property as {email: email}
+			_user: req.user.id,
+			dateSent: Date.now()
+		}
+
+		const mailer = new Mailer(survey, surveyTemplate(survey))
+	})
 }
