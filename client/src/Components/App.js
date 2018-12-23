36494226c9552729e1efce75f418080e542ec1b5
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-23 21:26:13 +0545

    add container class on outermost div in App.js

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Header from './Header'
+
 const Landing = () => <h1>Landing</h1>
 const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyView</h1>
 
 const App = () => {
 	return (
-		<div>
+		<div className="container">
 			<BrowserRouter>
 				<div>
 					<Header />
