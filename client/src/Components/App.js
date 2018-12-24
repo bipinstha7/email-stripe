fd23e1c5328821fe28e6ce1df4cb461e6e54b2ac
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-24 21:16:15 +0545

    import and use router

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header'
+const Landing = () => <h1>Landing</h1>
+const Dashboard = () => <h1>Dashboard</h1>
+const SurveyNew = () => <h1>SurveyView</h1>
 
 const App = () => {
 	return (
 		<div>
-			Hi There
-			<h1>nepal</h1>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+				</div>
+			</BrowserRouter>
 		</div>
 	)
 }
