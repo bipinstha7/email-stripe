0a0305de6f9918cb98cf8d1e0ab9037bae54fa21
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-05 20:43:13 +0545

    send status along with message

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-	res.send('Hello There')
+	res.status(200).send('Hello There')
 })
 
 app.listen(PORT, () => {
