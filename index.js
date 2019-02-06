c0db89cffdf0073e0895420b05eddd0f6494b53d
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-06 23:58:09 +0545

    remove dummy router

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-	res.status(200).send('Hello There')
-})
-
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`)
 })
