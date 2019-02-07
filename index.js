eca00a01db9db093455d68ff28f12c22c0b09716
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-07 21:12:53 +0545

    route to get profile information of a user

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
 		},
-		accessToken => {
+		(accessToken, refreshToken, profile, done) => {
 			console.log('accessToken:', accessToken)
+			console.log('refreshToken:', refreshToken)
+			console.log('profile:', profile)
 		}
 	)
 )
@@ -25,6 +27,8 @@ app.get(
 		scope: ['profile', 'email']
 	})
 )
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 
