fb7db47ab0e5d7d37dbc96265a78f2f4afeb9b35
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-11 22:13:38 +0545

    save new user to mongoose database. If the user already exists, then bypass the saving function and return the user

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 
 const keys = require('../config/keys')
+
+const User = mongoose.model('User')
 
 passport.use(
 	new GoogleStrategy(
@@ -11,9 +14,18 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken:', accessToken)
-			console.log('refreshToken:', refreshToken)
-			console.log('profile:', profile)
+			User.findOne({ googleId: profile.id })
+				.then(existingUser => {
+					if (existingUser) {
+						done(null, existingUser)
+					} else {
+						new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user))
+							.catch(err => console.log('Something went wrong while creating new user', err))
+					}
+				})
+				.catch(err => console.log('Error on finding given googleId', err))
 		}
 	)
 )
