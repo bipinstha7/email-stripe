ba7d141529dc45e3922b36a8e88ae1482521a6a0
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-12 20:38:47 +0545

    production and development keys & conditional configuration

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE.CLIENT.SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+}
