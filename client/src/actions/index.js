7b18b4d4c62d6ce281b0757e879c18aa48a2b2d6
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-14 23:09:10 +0545

    dispatch fetchUser action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { FETCH_USER } from './Types'
 
-const fetchUser = () => {
-	axios.get('/api/current_user')
+export const fetchUser = () => {
+	return dispatch => {
+		axios.get('/api/current_user').then(res => dispatch({ type: FETCH_USER, payload: res }))
+	}
 }
