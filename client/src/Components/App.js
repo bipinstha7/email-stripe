518cdd19451ae261e822fb807854668e1a83d005
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-14 23:08:23 +0545

    class based App.js Component and fetchUser in componetDidMount lifeCycle

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { fetchUser } from '../actions/index'
 
 import Header from './Header'
 
@@ -7,19 +9,28 @@ const Landing = () => <h1>Landing</h1>
 const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyView</h1>
 
-const App = () => {
-	return (
-		<div className="container">
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/surveys" component={Dashboard} />
-					<Route path="/surveys/new" component={SurveyNew} />
-				</div>
-			</BrowserRouter>
-		</div>
-	)
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser()
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		)
+	}
 }
 
-export default App
+export default connect(
+	null,
+	{ fetchUser }
+)(App)
