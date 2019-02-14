e756f3310e098a52c84a81b9ba302054a337db0d
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-14 23:30:59 +0545

    fetchuser auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,7 +3,7 @@ import { FETCH_USER } from '../actions/Types'
 export default function(state = {}, action) {
 	switch (action.type) {
 		case FETCH_USER:
-			return {}
+			return action.payload || false
 		default:
 			return state
 	}
