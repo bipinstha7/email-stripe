cc094c00bf200bd00db4c93c17fec12a18ab552a
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-15 23:56:18 +0545

    import Landing component in App.js file

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux'
 import { fetchUser } from '../actions/index'
 
 import Header from './Header'
+import Landing from './Landing'
 
-const Landing = () => <h1>Landing</h1>
 const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyView</h1>
 
