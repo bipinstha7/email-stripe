c4888be3f112d3d4eb9cbf637340daa5f5036ed5
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-17 14:57:20 +0545

    Use stripe checkout component in Header component

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
+import Payments from './Payments'
+
 class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
@@ -15,11 +17,14 @@ class Header extends Component {
 					</li>
 				)
 			default:
-				return (
-					<li>
+				return [
+					<li key="payments">
+						<Payments />
+					</li>,
+					<li key="logout">
 						<a href="/api/logout">Logout</a>
 					</li>
-				)
+				]
 		}
 	}
 
