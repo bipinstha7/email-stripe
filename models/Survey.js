fbfff63d5ccfb1edf149cc008387743ca991a049
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-19 21:41:10 +0545

    import and use RecipientSchema in survey model

--- models/Survey.js
+++ models/Survey.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
+const RecipientSchema = require('./Recipient')
+
 const SurveySchema = new Schema({
 	title: String,
 	subject: String,
 	body: String,
-	recipients: [String],
+	recipients: [RecipientSchema],
 	yes: {
 		type: Number,
 		default: 0
