cc3ef3a755fc66f04efa20ef5dbd012c2653223c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-20 21:43:30 +0545

    return html survey

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,13 +1,12 @@
 const keys = require('../../config/keys')
 
-module.exports = survey => {
-	return `
+module.exports = survey => `
         <html>
             <body>
                 <div style="text-align: center">
                     <h3>I'd like your input!</h3>
                     <p>Please answer the following question:</p>
-                    <p>${survey.body}</p>
+                    <p>Body: ${survey.body}</p>
                     <div>
                         <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
                     </div>
@@ -18,4 +17,3 @@ module.exports = survey => {
             </body>
         </html>
     `
-}
