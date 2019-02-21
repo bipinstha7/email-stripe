fe13080639d1849c910043b8b03dddaf93112ecb
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-21 19:31:22 +0545

    import Dashboard file in App.js

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -5,8 +5,8 @@ import { fetchUser } from '../actions/index'
 
 import Header from './Header'
 import Landing from './Landing'
+import Dashboard from './Dashboard'
 
-const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyView</h1>
 
 class App extends Component {
