aee4148ab050fedce0da36270ce4f2031c996210
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-22 23:59:24 +0545

    show review form data on SurveyFormReview

--- client/src/Components/surveys/SurveyFormReview.js
+++ client/src/Components/surveys/SurveyFormReview.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import formFields from './formFields'
+
 const SurveyFormReview = ({ onCancel, formValues }) => {
+	const reviewFields = formFields.map(({ label, name }) => {
+		return (
+			<div key={name}>
+				<label>{label}</label>
+				<div>{formValues[name]}</div>
+			</div>
+		)
+	})
+
 	return (
 		<div>
 			<h5>Please Confirm Your Entries</h5>
-
+			{reviewFields}
 			<button className="yellow darken-3 btn-flat" onClick={onCancel}>
 				Back
 			</button>
