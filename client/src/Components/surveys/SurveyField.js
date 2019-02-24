a5aa85647f91a74fdfbdb27f14e94e056eafc016
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-24 21:00:38 +0545

    add htmlFor attribute in label tag

--- client/src/Components/surveys/SurveyField.js
+++ client/src/Components/surveys/SurveyField.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default ({ input, label, meta: { error, touched } }) => {
 	return (
 		<div>
-			<label>{label}</label>
-			<input {...input} style={{ marginButtom: '5px' }} />
+			<label htmlFor={label}>{label}</label>
+			<input {...input} style={{ marginButtom: '5px' }} id={label} />
 			<div className="red-text" style={{ marginButtom: '20px' }}>
 				{touched && error}
 			</div>
