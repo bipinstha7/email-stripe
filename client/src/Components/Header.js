63cbc7f91872d10eb5c80f7d0199d4b1f42dee80
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-26 20:42:07 +0545

    change Header name to e-Emaily

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -36,7 +36,7 @@ class Header extends Component {
 			<nav>
 				<div className="nav-wrapper">
 					<Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
-						Emaily
+						e-Emaily
 					</Link>
 					<ul className="right">{this.renderContent()}</ul>
 				</div>
