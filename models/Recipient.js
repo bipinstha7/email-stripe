b35f68a483f47a8ea3625d01d1ab60f54d082b1d
bipinstha7 <bipin.shrestha901@gmail.com>
2019-02-26 20:37:29 +0545

    recipientSchema has been used as a subdocument in Survey Schema

--- models/Recipient.js
+++ models/Recipient.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
+/* recipientSchema has been used as a subdocument in Survey Schema */
 const recipientSchema = new Schema({
 	email: String,
 	responded: {
